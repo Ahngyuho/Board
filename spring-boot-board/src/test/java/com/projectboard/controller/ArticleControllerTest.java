aab9e15007c5580205205c307a3033f83a56a979
Ahngyuho <gyuho0897@gmail.com>
2022-08-06 03:37:50 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메서드로 테스트를 통과하게 열어주고 뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 
    부가적인 옵션이 들어갈 수 있으니
    적절히 테스트 메서드 수정
    contentTypeCompatableWith(MediaType.
    TEXT_HTML)) 이 부분

--- spring-boot-board/src/test/java/com/projectboard/controller/ArticleControllerTest.java
+++ spring-boot-board/src/test/java/com/projectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 {게시판} 페이지 - 정상 호출")
     @Test
     void giveNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -32,7 +31,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 //view 이름도 테스트 할 수 있음 디렉토리도 적어줘야함 article(디렉토리)/index(html)
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 //modelattribute 가 있는지 없는지 검사 이 articles라는 key값이 있는지
