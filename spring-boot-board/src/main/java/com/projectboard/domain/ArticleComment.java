e9292ba40cbd3971a05b6973131183d5c0289c67
Ahngyuho <gyuho0897@gmail.com>
2022-08-09 19:55:29 +0900

    #18 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- spring-boot-board/src/main/java/com/projectboard/domain/ArticleComment.java
+++ spring-boot-board/src/main/java/com/projectboard/domain/ArticleComment.java
@@ -3,11 +3,7 @@ package com.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,7 +11,7 @@ import java.util.Objects;
 
 //댓글
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -29,6 +25,8 @@ public class ArticleComment extends AuditingFields{
     @Setter @ManyToOne(optional = false) // optional = false 면 이건 필수값이라는 뜻임
                                 //cascade 는 none 댓글을 삭제해도 게시글에 영향가지 않도록 하기 위함
     private Article article;
+
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;
     @Setter @Column(nullable = false,length = 500) private String content;
 
 
@@ -36,14 +34,15 @@ public class ArticleComment extends AuditingFields{
 
     }
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
     //factory method
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article,content);
+    public static ArticleComment of(Article article,UserAccount userAccount ,String content) {
+        return new ArticleComment(article,userAccount,content);
     }
 
     @Override
--- spring-boot-board/src/main/java/com/projectboard/domain/UserAccount.java
+++ spring-boot-board/src/main/java/com/projectboard/domain/UserAccount.java
@@ -10,28 +10,28 @@ import java.util.Objects;
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "userId"),
-        @Index(columnList = "email",unique = true),
+        @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
 @Entity
-public class UserAccount extends AuditingFields{
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class UserAccount extends AuditingFields {
 
-    @Setter @Column(nullable = false,length = 50) private String userId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter @Column(length = 50) private String userId;
+
     @Setter @Column(nullable = false) private String userPassword;
 
     @Setter @Column(length = 100) private String email;
     @Setter @Column(length = 100) private String nickname;
     @Setter private String memo;
 
-    protected UserAccount(){
 
-    }
+    protected UserAccount() {}
 
-    public UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
@@ -39,19 +39,20 @@ public class UserAccount extends AuditingFields{
         this.memo = memo;
     }
 
-    public UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId,userPassword,email,nickname,memo);
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccount(userId, userPassword, email, nickname, memo);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof UserAccount userAccount)) return false;
-        return id != null && id.equals(userAccount.id);
+        if (!(o instanceof UserAccount that)) return false;
+        return userId != null && userId.equals(that.getUserId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(userId);
     }
+
 }
