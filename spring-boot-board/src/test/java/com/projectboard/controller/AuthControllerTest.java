6603e3a607d4fabde17fdd53c8a7c273c76e7336
agh <gyuho0897@gmail.com>
2023-11-05 20:24:17 +0900

    feat: 설정 코드 작성

--- spring-boot-board/src/test/java/com/projectboard/controller/AuthControllerTest.java
+++ spring-boot-board/src/test/java/com/projectboard/controller/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.projectboard.controller;
 
 import com.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@Disabled
 @DisplayName("view 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
 @WebMvcTest
