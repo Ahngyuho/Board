b2b80fb69d1ba385ee5ae5a0b2743b415a1c8bb5
agh <gyuho0897@gmail.com>
2023-11-05 20:19:20 +0900

    #26 유저 도메인 코드 작성

--- spring-boot-board/src/main/java/com/projectboard/repository/UserAccountRepository.java
+++ spring-boot-board/src/main/java/com/projectboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.projectboard.repository;
 import com.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount,String> {
 }
